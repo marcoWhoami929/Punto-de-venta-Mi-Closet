e325bf4402f642977bba0b53606aed85a799a40d
MARCO <mm_marco_mar@hotmail.com>
2024-10-10 18:18:19 -0600

    Sale Controller Db

--- DB/sistemapos.sql
+++ DB/sistemapos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-10-2024 a las 02:19:56
+-- Tiempo de generación: 11-10-2024 a las 02:17:41
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 7.4.33
 
